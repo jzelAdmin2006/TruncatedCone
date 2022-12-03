55ce94f823433032c04639418f5d5ecc147b88bd
jzelAdmin <johannes_zeller@sluz.ch>
2022-12-03 09:50:06 +0100

    test truncated cone calculations

--- src/test/java/tech/bison/trainee2021/tasksBlockSix/TruncatedConeTest.java
+++ src/test/java/tech/bison/trainee2021/tasksBlockSix/TruncatedConeTest.java
@@ -1,5 +1,43 @@
 package tech.bison.trainee2021.tasksBlockSix;
 
-public class TruncatedConeTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
+public class TruncatedConeTest {
+  @Test
+  void newTruncatedCone_getSurface_isCorrect() {
+    TruncatedCone f1 = new TruncatedCone(12, 5.3, 7.5);
+
+    double result = f1.getSurface();
+
+    assertThat(result).isEqualTo(1087.2213542281866);
+  }
+
+  @Test
+  void newTruncatedCone_getSeathing_isCorrect() {
+    TruncatedCone f1 = new TruncatedCone(12, 5.3, 7.5);
+
+    double result = f1.getSeathing();
+
+    assertThat(result).isEqualTo(546.5846744719191);
+  }
+
+  @Test
+  void newTruncatedCone_getSeathLine_isCorrect() {
+    TruncatedCone f1 = new TruncatedCone(12, 5.3, 7.5);
+
+    double result = f1.getSeathLine();
+
+    assertThat(result).isEqualTo(10.056838469419702);
+  }
+
+  @Test
+  void newTruncatedCone_getVolume_isCorrect() {
+    TruncatedCone f1 = new TruncatedCone(12, 5.3, 7.5);
+
+    double result = f1.getVolume();
+
+    assertThat(result).isEqualTo(1851.1049313114456);
+  }
 }
